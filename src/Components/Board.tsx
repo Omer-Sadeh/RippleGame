93aee9bd7ad22a9fdd7d6c7d325f01b4177c3288
OmerSadeh <88449326+Omer-Sadeh@users.noreply.github.com>
2021-11-16 15:34:57 +0200

    Change restrictions

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -10,14 +10,14 @@ function Board() {
 
     const updateBoard = (Size: number, newSquares: boolean[]) => {
       var newSize = SIZE
-      if (Size > 2) {
+      if (Size > 1) {
         newSize = Size
         setSIZE(newSize);
         setIndexArray(Array.from(Array(newSize).keys()));
         setSquares(newSquares);
         setSquaresClass(ExtractClassArray(newSquares));
       }
-      else alert("Cannot create board smaller than 3x3! try again.");
+      else alert("Cannot create board smaller than 2x2! try again.");
     }
 
     const ExtractClassArray = (currentArray: boolean[]) => {
@@ -37,7 +37,7 @@ function Board() {
         if (!(index < 0) && !(index > SIZE * SIZE) && (Math.abs(index%SIZE-i%SIZE) < 2)) tempSquares[index] = !tempSquares[index];
       });
 
-      updateBoard(SIZE, tempSquares)
+      updateBoard(SIZE, tempSquares);
       if (tempSquares === winningBoard) winSequence();
     }
 
@@ -45,7 +45,7 @@ function Board() {
       var winSquares = Array(SIZE * SIZE).fill("squareWon");
       await timeout(100);
       updateBoard(SIZE, winSquares);
-      await timeout(500);
+      await timeout(3000);
       updateBoard(SIZE, Array(SIZE * SIZE).fill(false));
     }
     function timeout(delay: number) {return new Promise( res => setTimeout(res, delay) );}
